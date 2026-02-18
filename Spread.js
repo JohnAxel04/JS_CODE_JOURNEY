7cb412e443f555e4e45c18637d53f0578acf4fc6
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2026-02-18 15:21:21 +0800

    Using spread operator

---
+++ Spread.js
@@ -0,0 +1,9 @@
+// let myName = "John Axel";
+// let lagayan = [...myName].join("-");
+// console.log(lagayan)
+
+let vegetable = ["carrots","broccoly","gulay"];
+let fruits = ["apple","banana","orange"];
+let combine = [...vegetable, ...fruits, "milk"];
+
+console.log(combine)
